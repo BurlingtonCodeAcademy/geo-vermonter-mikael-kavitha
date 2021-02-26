5730a66a2d2f80fea9e16cc59acaf8cc5eccb884
Kavitha <c.kavitha1987@gmail.com>
2021-02-26 11:01:37 -0500

    map works

--- src/components/scripts/RandomStart.js
+++ src/components/scripts/RandomStart.js
@@ -1,11 +1,12 @@
-import borderData from './border'
+/*import borderData from './border'
 import leafletPip from 'leaflet-pip'
 import L from "leaflet"
-
+import { useState } from "react";
 
 
 function RandomStart(){
-    
+    const [center, setCenter] = useState([43.88, -72.7317]);
+    const[zoom,setZoom]=useState(8)
     //start by definig variables for max and min long and lat
     const vtMinLat = 42.7289
     const vtMaxLat = 45.0153
@@ -19,14 +20,53 @@ let longRandom= (Math.random()*(vtMaxLong-vtMinLong)) + vtMinLong;
 
 
 
-const layerLength=leafletPip.pointInLayer([longRandom,latRandom],vtBorderData)
+const layerLength=leafletPip.pointInLayer([longRandom,latRandom],vtBorderData),true
 if(layerLength){
     return {lat:latRandom, long:longRandom}
 }else{
-    return layerLength
+    return RandomStart()
 }
 
 
 }
 
-export default RandomStart 
+export default RandomStart */
+
+/*function App() {
+  const [start, setStart] = useState(true);
+  const [guess, setGuess] = useState(false);
+  const [quit, setQuit] = useState(false);
+  const [buttonState, setButtonState] = useState(false);
+
+  function startClickHandler() {
+    setStart(false);
+    guessClickHandler(true);
+    quitClickHandler(true);
+    setButtonState(!buttonState);
+    
+  }
+
+  //enables the guess click after start button is clicked
+  function guessClickHandler() {
+    setButtonState(!buttonState)
+  }
+
+  function quitClickHandler() {
+    setButtonState(!buttonState)
+  }
+
+  const [center, setCenter] = useState([43.88, -72.7317]);
+
+  return (
+    <>
+      <Map center={center} />
+      <HandleClick
+        startClickHandler={startClickHandler}
+        guessClickHandler={guessClickHandler}
+        quitClickHandler={quitClickHandler}
+        buttonState={buttonState}
+      />
+    </>
+  );
+}
+*/
