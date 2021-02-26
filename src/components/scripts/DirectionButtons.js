cda78fa66d9cec306927f30899784c02b08ddd72
Kavitha <c.kavitha1987@gmail.com>
2021-02-26 16:19:11 -0500

    avoid this commit

--- src/components/scripts/DirectionButtons.js
+++ src/components/scripts/DirectionButtons.js
@@ -2,14 +2,20 @@
 // buttons that helps player to move in different directions
 
 function DirectionButtons(props){
-
-
+const[center,setCenter]=useState([43.88, -72.7317])
+const[zoom,setZoom]=useState(8)
+const[viewCenter,setViewCenter]=useState([43.88, -72.7317])
+const[score,setScore]=useState(100)
+let scoreChange=setScore(score-1)
+let startPosition=setCenter
+let longInput=props.long
+let latInput=props.lat
 return(
     <>
-    <button onClick={props.DirectionButtons}{scoreChange}>North</button>
-    <button onClick={props.DirectionButtons}>South</button>
-    <button onClick={props.DirectionButtons}>East</button>
-    <button onClick={props.DirectionButtons}>West</button>
+    <button onClick={props.moveNorth}{scoreChange}>North</button>
+    <button onClick={props.moveSouth}>South</button>
+    <button onClick={props.moveEast}>East</button>
+    <button onClick={props.moveWest}>West</button>
     <button onClick={props.startPosition}>Home</button>
     </>
 )
--- src/components/scripts/InfoBar.js
+++ src/components/scripts/InfoBar.js
@@ -10,10 +10,8 @@ function InfoBar() {
     const [town, setTown] = useState('?')
     return (
         <>
-            <div>Longitude:{longitude}</div>
-            <div>Latitude:{latitude}</div>
-            <div>County:{county}</div>
-            <div>Town:{town}</div>
+            <div>Longitude:{longitude}Latitude:{latitude}County:{county}Town:{town}</div>
+           
 
 
 
